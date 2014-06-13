7c516e047c4a6fd65ea22a5f1d210197bafe73d1
liechtenstein <bkm71@hotmail.com>
2014-06-13 13:20:03 -0500

    Code cleanup (unused/commented out) code is removed

--- matlab-code/get_intersect_array.m
+++ matlab-code/get_intersect_array.m
@@ -2,6 +2,5 @@
 function result_array = get_intersect_array(timestamp_array,value_matrix)
   [arr,index_1,index_2] = intersect(timestamp_array,value_matrix(:,1));
   result_array = value_matrix(index_2,:);
-%  result_array = result_array(:,2);
   result_array(:,1) = [];
 end
